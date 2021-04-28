7340fbd5d9870ea7f8f6e3d1c457dd2d07fbe74a
codelaghien <codelaghien.github.io>
2021-04-28 16:33:41 -0700

    Queue dùng Array

--- Main.java
+++ Main.java
@@ -3,29 +3,29 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.print("Queue - Java \n"); 
+		System.out.print("Queue - Java \n");
 		
-		MyQueue stack = new MyQueue(3);
+		MyQueue queue = new MyQueue(3);
 
-		stack.enqueue(new Node(100));
-		stack.enqueue(new Node(200));
-		stack.enqueue(new Node(300));
-		System.out.print("\n");  
-		stack.print();
+		queue.enqueue(new Node(100));
+		queue.enqueue(new Node(200));
+		queue.enqueue(new Node(300));
+		System.out.print("\n");
+		queue.print();
 		System.out.print("\n"); 
 		System.out.print("\n"); 
-		stack.enqueue(new Node(400));
+		queue.enqueue(new Node(400));
 		
-		Node node = stack.dequeue();
+		Node node = queue.dequeue();
 		if (node != null) {
-			System.out.print("dequeue\n");  
+			System.out.print("dequeue\n");
 			node.printData();
 		} 
 		
-		stack.enqueue(new Node(400));
+		queue.enqueue(new Node(400));
 
 		System.out.print("\n");
-		stack.print();
+		queue.print();
 	}
 	
 }
